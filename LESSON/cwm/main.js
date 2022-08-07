2775e81a9e6bb5338634de6bff22574b60503d66
Aizuki69 <110227441+Aizuki69@users.noreply.github.com>
2022-08-07 16:00:38 +0700

    them file cwn

---
+++ LESSON/cwm/main.js
@@ -0,0 +1,6 @@
+var song = document.getElementsByClassName(".b1");
+var playbtn = document.querySelector(".pu1");
+playbtn.addEventListener("click", playPause);
+function playPause () {
+    console.log("hi")
+}
