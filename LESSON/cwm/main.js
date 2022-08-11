27f678aeb9a45f165e9762555f9418e4b11e3fd8
Aizuki69 <110227441+Aizuki69@users.noreply.github.com>
2022-08-11 22:03:00 +0700

    thêm web cuối khóa trước

--- LESSON/cwm/main.js
+++
@@ -1,6 +0,0 @@
-var song = document.getElementsByClassName(".b1");
-var playbtn = document.querySelector(".pu1");
-playbtn.addEventListener("click", playPause);
-function playPause () {
-    console.log("hi")
-}
